5d52d408c1cf797c9707c304e235096f99e010e6
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-04-12 20:57:37 -0300

    Exercício soma de valores

--- exercício-soma-de-valores/src/ExercicioSoma.java
+++ exercício-soma-de-valores/src/ExercicioSoma.java
@@ -1,3 +1,9 @@
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+Entrada:                       Saída:
+10                             SOMA = 40
+30*/
+
 import java.util.Scanner;
 
 public class ExercicioSoma {
