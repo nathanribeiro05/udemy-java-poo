ba7500c5a98e942d04339971c38b553fa140c67a
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-04-14 16:47:19 -0300

    calculo estoque com construtor

---
+++ exercicio-construtor/src/Estoque.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Estoque {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String nome = sc.nextLine();
+        System.out.print("Price: ");
+        double preco = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantidade = sc.nextInt();
+        Produto produto = new Produto(nome, preco, quantidade); //Variável produto já é criada com definição.
+
+        System.out.println();
+        System.out.println("Product data: " + produto);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        produto.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + produto);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        produto.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + produto);
+
+        sc.close();
+    }
+}
---
+++ exercicio-construtor/src/Produto.java
@@ -0,0 +1,37 @@
+public class Produto {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Produto(String name, double price, int quantity) { //Construtor criado para predefinir os atributos do produto
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValueInStock() {
+        return quantity * price;
+         
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity; //this. especifica que está se referindo ao 
+                                   //atributo quantity da classe (public int quantity) e não ao parâmetro do método. 
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name 
+        + ", $" 
+        + String.format("%.2f", price) 
+        + ", " 
+        + quantity 
+        + " units, Total: $" 
+        +  String.format("%.2f", totalValueInStock());
+    }
+    
+}
