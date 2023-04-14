bf754dcdbb2676a899fa4d33e3b4e6e9e45d57fe
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-04-14 13:11:38 -0300

    calculo retangulo java

---
+++ calculo-retangulo/src/Aplicacao.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*Fazer um programa para ler os valores da largura e altura
+de um retângulo. Em seguida, mostrar na tela o valor de
+sua área, perímetro e diagonal.
+Exemplo: 
+Enter rectangle width and height:
+3.00
+4.00
+AREA = 12.00
+PERIMETER = 14.00
+DIAGONAL = 5.00 */
+
+public class Aplicacao {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Retangulo retangulo = new Retangulo();
+        System.out.println("Enter rectangle width and height: ");
+        retangulo.width = sc.nextDouble();
+        retangulo.height = sc.nextDouble();
+
+        System.out.println("AREA = " + String.format("%.2f", retangulo.area()));
+        System.out.println("PERIMETER = " + String.format("%.2f",retangulo.perimeter()));
+        System.out.println("DIAGONAL = " + String.format("%.2f",retangulo.diagonal()));
+
+
+
+        sc.close();
+    }
+}
